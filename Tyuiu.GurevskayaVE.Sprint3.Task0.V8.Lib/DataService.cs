16336c9f3fcc4192569bf8956311edf8b8b14853
Tyuiu.GurevskayaVE@outlook.com <Tyuiu.GurevskayaVE@outlook.com>
2023-10-16 18:40:50 +0500

    task1

--- Tyuiu.GurevskayaVE.Sprint3.Task0.V8.Lib/DataService.cs
+++ Tyuiu.GurevskayaVE.Sprint3.Task0.V8.Lib/DataService.cs
@@ -8,16 +8,16 @@ using tyuiu.cources.programming.interfaces.Sprint3;
 namespace Tyuiu.GurevskayaVE.Sprint3.Task0.V8.Lib
 {
     public class DataService : ISprint3Task0V8
-    {
-        public double GetSumSeries(double value)
+    { 
+        public double GetSumSeries(double value, int startValue, int stopValue)
         {
-            
             double res = 1;
-            for (int k = 1; k<=10; k++)
+            int k;
+            for (k = startValue; k<=stopValue; k++)
             {
                 res *= (Math.Pow(value, k) + 1) * Math.Cos(4);
             }
-            return Math.Round(res,3);
+            return Math.Round(res, 3);
         }
     }
 }
--- Tyuiu.GurevskayaVE.Sprint3.Task0.V8.Test/DataServiceTest.cs
+++ Tyuiu.GurevskayaVE.Sprint3.Task0.V8.Test/DataServiceTest.cs
@@ -11,8 +11,10 @@ namespace Tyuiu.GurevskayaVE.Sprint3.Task0.V8.Test
         public void TestMethod1()
         {
             DataService ds = new DataService();
+            int startValue = 1;
+            int stopValue = 10;
             double value = 0.25;
-            double res = ds.GetSumSeries(value);
+            double res = ds.GetSumSeries(value, startValue, stopValue);
             double wait = 0.019;
             Assert.AreEqual(wait, res);
         }
