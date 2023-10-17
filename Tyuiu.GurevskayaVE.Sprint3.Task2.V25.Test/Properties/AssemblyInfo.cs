0aa23f762ad0356bb15d1a54ddf2e55e8162c69e
Tyuiu.GurevskayaVE@outlook.com <Tyuiu.GurevskayaVE@outlook.com>
2023-10-17 19:10:00 +0500

    task2

---
+++ Tyuiu.GurevskayaVE.Sprint3.Task2.V25.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.GurevskayaVE.Sprint3.Task2.V25.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.GurevskayaVE.Sprint3.Task2.V25.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("579310df-cb92-43a9-8b19-d04bf13ef4df")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
